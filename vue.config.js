593a737f5e79c1d0631c9beb335793b8a58b8095
mastep <igorchurikov@ya.ru>
2022-09-27 00:36:37 +0300

    Исправлена ошибка cors

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    host: '188.225.42.19',
+    // https:true,
+    port: 8080,
+    client: {
+      webSocketURL: 'ws://188.225.42.19:8080/ws',
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    }
+  },
   transpileDependencies: true
 })
